87986b1d3bf9dab5ec6adbfc4fae31014f272341
Filip Oreč <filip.orec123@gmail.com>
2018-10-23 21:18:41 +0200

    Dodana dokumentacija

--- NumericalOptimization/src/main/java/hr/fer/zemris/optjava/dz2/IHFunction.java
+++ NumericalOptimization/src/main/java/hr/fer/zemris/optjava/dz2/IHFunction.java
@@ -4,6 +4,13 @@ import Jama.Matrix;
 
 public interface IHFunction extends IFunction{
 	
+	/**
+	 * Calculates Hesse-Matrix for given input. Input x 
+	 * must be a vector with dimensions (N x 1) where N is 
+	 * function's number of variables. 
+	 * @param x Input vector
+	 * @return Hesse-Matrix for given input
+	 */
 	public Matrix getHesseMatrix(Matrix x);
 
 }
